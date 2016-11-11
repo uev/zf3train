9e656dfab0e944e769b670ac4a9f23bb1576266f
uev <uev5bsd@fmail.com>
2016-11-11 17:09:46 +0200

    doctrine dependency and friends

--- config/modules.config.php
+++ config/modules.config.php
@@ -11,7 +11,13 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
+    'Zend\Cache',
+    'Zend\Form',
+    'Zend\InputFilter',
+    'Zend\Filter',
     'Zend\Router',
     'Zend\Validator',
     'Application',
+    'DoctrineModule',
+    'DoctrineORMModule',
 ];
