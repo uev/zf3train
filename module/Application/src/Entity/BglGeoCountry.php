8f24b1150a7300a7d3870f1fb2b1c0cc04417e83
uev <uev5bsd@fmail.com>
2016-11-16 12:31:22 +0700

    Entities

---
+++ module/Application/src/Entity/BglGeoCountry.php
@@ -0,0 +1,41 @@
+<?php
+
+
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * BglGeoCountry
+ *
+ * @ORM\Table(name="bgl_geo_country")
+ * @ORM\Entity
+ */
+class BglGeoCountry
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="SEQUENCE")
+     * @ORM\SequenceGenerator(sequenceName="bgl_geo_country_id_seq", allocationSize=1, initialValue=1)
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="code", type="string", length=2, nullable=false)
+     */
+    private $code = '0';
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=128, nullable=false)
+     */
+    private $name = '';
+
+
+}
+
---
+++ module/Application/src/Entity/BglGeoRegion.php
@@ -0,0 +1,41 @@
+<?php
+
+
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * BglGeoRegion
+ *
+ * @ORM\Table(name="bgl_geo_region", indexes={@ORM\Index(name="bgl_geo_region_countryid_idx", columns={"countryid"}), @ORM\Index(name="bgl_geo_region_name_idx", columns={"name"})})
+ * @ORM\Entity
+ */
+class BglGeoRegion
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="SEQUENCE")
+     * @ORM\SequenceGenerator(sequenceName="bgl_geo_region_id_seq", allocationSize=1, initialValue=1)
+     */
+    private $id;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="countryid", type="integer", nullable=false)
+     */
+    private $countryid = '0';
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=64, nullable=false)
+     */
+    private $name = '';
+
+
+}
+
